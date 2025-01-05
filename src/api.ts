d592dd98c65e934db7d475a7319be0f6f3ef4f3e
antonin fille <antonin.fille@epitech.eu>
2025-01-05 14:57:58 +0100

    add: change api endpoint for server endpoint

--- src/api.ts
+++ src/api.ts
@@ -1,5 +1,6 @@
 export async function getProductInfo(barcode: string): Promise<any> {
-    const url = `https://world.openfoodfacts.net/api/v2/product/${barcode}`;
+    console.log("API call to http://127.0.0.1:8080/api/v1/aletheia/products/")
+    const url = `http://127.0.0.1:8080/api/v1/aletheia/products/${barcode}`;
     const response = await fetch(url);
     return response.json();
 }
